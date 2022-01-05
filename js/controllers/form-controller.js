dce7d0fd3f326f33363b6b6c3358bf55c786d85a
Victor Abreu <vlabreu@outlook.com>
2022-01-04 21:11:59 -0300

    Integração API ViaCep pronta

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,66 +1,85 @@
-import Address from '../models/address.js';
-import * as requestService from '../services/request-service.js';
+import Address from "../models/address.js";
+import * as addressService from "../services/address-service.js";
 
-function State(){
-    this.address = new Address();
+function State() {
+  this.address = new Address();
 
-    this.btnSave = null;
-    this.btnClear = null;
-    this.inputCep = null;
-    this.inputStreet = null;
-    this.inputNumber = null;
-    this.inputCity = null;
-    this.errorCep = null;
-    this.errorNumero = null;
+  this.btnSave = null;
+  this.btnClear = null;
+  this.inputCep = null;
+  this.inputStreet = null;
+  this.inputNumber = null;
+  this.inputCity = null;
+  this.errorCep = null;
+  this.errorNumero = null;
 }
 
 const state = new State();
 
-export function init(){
-    state.inputCep = document.forms.newAddress.cep;
-    state.inputStreet = document.forms.newAddress.logradouro;
-    state.inputNumber = document.forms.newAddress.numero;
-    state.inputCity = document.forms.newAddress.cidade;
-    state.errorCep = document.querySelector('[data-error="cep"]')
-    state.errorNumero = document.querySelector('[data-error="numero"]')
-    state.btnSave = document.forms.newAddress.btnSave;
-    state.btnClear = document.forms.newAddress.btnClear;
+export function init() {
+  state.inputCep = document.forms.newAddress.cep;
+  state.inputStreet = document.forms.newAddress.logradouro;
+  state.inputNumber = document.forms.newAddress.numero;
+  state.inputCity = document.forms.newAddress.cidade;
+  state.errorCep = document.querySelector('[data-error="cep"]');
+  state.errorNumero = document.querySelector('[data-error="numero"]');
+  state.btnSave = document.forms.newAddress.btnSave;
+  state.btnClear = document.forms.newAddress.btnClear;
 
-    state.inputNumber.addEventListener('change', handleInputNumberChange);
-    state.btnClear.addEventListener('click', handleBtnCleanClick);
-    state.btnSave.addEventListener('click', handleBtnSaveClick);
+  state.inputNumber.addEventListener("change", handleInputNumberChange);
+  state.inputCep.addEventListener("change", handleInputCepChange);
+  state.btnClear.addEventListener("click", handleBtnCleanClick);
+  state.btnSave.addEventListener("click", handleBtnSaveClick);
 }
 
-async function handleBtnSaveClick(event){
-    event.preventDefault();
-    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/');
-    console.log(result);
-}
-
-function handleInputNumberChange(event){
-    if(event.target.value == ""){
-        setFormError("numero", "Campo requerido");
-    }else{
-        setFormError("numero", "");
-    }
-}
-
-function handleBtnCleanClick(event){
-    event.preventDefault();
-    clearForm();
-}
-
-function clearForm(){
-    state.inputCep.value = "";
-    state.inputStreet.value = "";
-    state.inputNumber.value = "";
-    state.inputCity.value = "";
+async function handleInputCepChange(event) {
+  const cep = event.target.value;
+  try {
+    const address = await addressService.findByCep(cep);
+    state.inputStreet.value = address.logradouro;
+    state.inputCity.value = address.cidade;
+    state.address = address;
     setFormError("cep", "");
-    setFormError("numero", "");
-    state.inputCep.focus();
+    state.inputNumber.focus();
+  } catch (e) {
+    state.inputStreet.value = "";
+    state.inputCity.value = "";
+    setFormError("cep", "Informe um CEP válido");
+  }
 }
 
-function setFormError(key, value){
-    const element = document.querySelector(`[data-error="${key}"]`);
-    element.innerHTML = value;
+async function handleBtnSaveClick(event) {
+  event.preventDefault();
+  const result = await requestService.getJson(
+    "https://viacep.com.br/ws/01001000/json/"
+  );
+  console.log(result);
+}
+
+function handleInputNumberChange(event) {
+  if (event.target.value == "") {
+    setFormError("numero", "Campo requerido");
+  } else {
+    setFormError("numero", "");
+  }
+}
+
+function handleBtnCleanClick(event) {
+  event.preventDefault();
+  clearForm();
+}
+
+function clearForm() {
+  state.inputCep.value = "";
+  state.inputStreet.value = "";
+  state.inputNumber.value = "";
+  state.inputCity.value = "";
+  setFormError("cep", "");
+  setFormError("numero", "");
+  state.inputCep.focus();
+}
+
+function setFormError(key, value) {
+  const element = document.querySelector(`[data-error="${key}"]`);
+  element.innerHTML = value;
 }
